694f7231ea012b9520878e332565ec3807006af0
kwintenvdb <kwintenvdberghe@gmail.com>
2020-10-09 19:29:55 +0200

    Use craco to allow more detailed postcss config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Overview from './components/Overview';
 import './styles/main.scss';
+import './styles/tailwind.css';
 
 function App() {
     return (
         <div className="container">
+            <h1>Unity Publisher Client</h1>
+
             <Overview></Overview>
         </div>
     );

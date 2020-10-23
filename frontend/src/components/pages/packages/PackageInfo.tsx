7bf9ebe371c54234ed189ccb1b454c261083bfb1
kwintenvdb <kwintenvdberghe@gmail.com>
2020-10-23 19:28:47 +0200

    Refined package info component

--- frontend/src/components/pages/packages/PackageInfo.tsx
+++ frontend/src/components/pages/packages/PackageInfo.tsx
@@ -5,6 +5,7 @@ import React, { FunctionComponent } from 'react';
 import { Card } from 'src/components/common/Card';
 import { formatCurrency } from 'src/utils/formatCurrency';
 import { PackageData } from 'unity-publisher-api';
+import { H4 } from 'baseui/typography';
 
 type Props = {
     pkg: PackageData;
@@ -22,12 +23,18 @@ export const PackageInfo: FunctionComponent<Props> = ({ pkg }) => {
                 <div className="text-base font-normal"><Link href={pkg.url}>View in Asset Store</Link></div>
             </div>
         }>
-            <div>{formatCurrency(pkg.price)}</div>
-            <div>
+            <div className="w-full flex">
+                <div className="w-1/4">
+                    <H4>{formatCurrency(pkg.price)}</H4>
+                    <p>Price</p>
+                </div>
+                <div>
+                    <H4>
+                        <StarRating value={pkg.averageRating || 0} />
+                    </H4>
+                    <p>Average Rating ({pkg.numRatings} reviews)</p>
 
-            </div>
-            <div>
-                Rating: {<StarRating value={pkg.averageRating || 0} />} ({pkg.numRatings} ratings)
+                </div>
             </div>
         </Card>
     );

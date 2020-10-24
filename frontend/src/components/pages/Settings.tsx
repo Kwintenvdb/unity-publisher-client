9d59ee1eb04f7478c36e5d25d73b8abb17f0f287
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2020-10-24 21:17:19 +0200

    Removed comments from settings page

--- frontend/src/components/pages/Settings.tsx
+++ frontend/src/components/pages/Settings.tsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import superagent from 'superagent';
-import { Card } from '../common/Card';
 import { Button } from 'baseui/button';
-import { Checkbox, STYLE_TYPE } from 'baseui/checkbox';
-import { Notification, KIND } from 'baseui/notification';
-import { useMutation, useQuery, useQueryCache } from 'react-query';
-import { isSubscribedToEmailAlerts, subscribeToEmailAlerts, unsubscribeFromEmailAlerts } from 'src/api';
 import { FormControl } from 'baseui/form-control';
 import { Input } from 'baseui/input';
-import { createFormOverrides } from 'src/overrides/InputOverrides';
+import { KIND, Notification } from 'baseui/notification';
+import React from 'react';
 import { useForm } from 'react-hook-form';
-
-// tido formdata
+import { useMutation, useQuery, useQueryCache } from 'react-query';
+import { isSubscribedToEmailAlerts, subscribeToEmailAlerts, unsubscribeFromEmailAlerts } from 'src/api';
+import { createFormOverrides } from 'src/overrides/InputOverrides';
+import { Card } from '../common/Card';
 
 type FormData = {
     email: string;
@@ -35,50 +31,6 @@ export function Settings() {
     });
 
     const emailFormError = () => !!errors.email;
-
-    // useEffect(() => {
-    //     isSubscribedToPush().then(setSubscribed);
-    // }, []);
-
-    // const isSubscribedToPush = async () => {
-    //     if ('serviceWorker' in navigator) {
-    //         const sw = await navigator.serviceWorker.ready;
-    //         const sub = await sw.pushManager.getSubscription();
-    //         return sub !== null;
-    //     }
-    //     return false;
-    // };
-
-    // const enableNotifications = async () => {
-    //     console.log('enabling notifications');
-
-    //     const sw = await navigator.serviceWorker.ready;
-    //     console.log(sw);
-
-    //     const publicKey = 'BE8WIaYS7oi4LE1nONDmiZ_qoUPw-H4CKC8B8ncRCSLguT0n69yKB0sLnwRzEv7rUOJJR_FBdyo4En4nuehhwak';
-    //     const sub = await sw.pushManager.subscribe({
-    //         userVisibleOnly: true,
-    //         applicationServerKey: publicKey
-    //     });
-    //     setSubscribed(true);
-    //     console.log(sub.toJSON());
-
-    //     superagent.post('/api/notifications/subscribe')
-    //         .send(sub.toJSON())
-    //         .then(() => {
-    //             console.log('sent subscription');
-    //         });
-    // };
-
-    // const disableNotifications = async () => {
-    //     console.log('disabling notifications');
-    //     const sw = await navigator.serviceWorker.ready;
-    //     const sub = await sw.pushManager.getSubscription();
-    //     await sub?.unsubscribe();
-    //     await superagent.post('/api/notifications/unsubscribe')
-    //         .then(() => console.log('sent unsubscription'));
-    //     setSubscribed(false);
-    // };
 
     const enableAlerts = (formData: FormData) => {
         subscribeMutation(formData.email);
@@ -122,18 +74,6 @@ export function Settings() {
                         : <Button type="submit">Enable Email Alerts</Button>
                     }
                 </form>
-
-                {/* <Input */}
-                {/* TODO make the button appear to request permission to show notifications */}
-
-
-                {/* TODO use the toggle to switch notifications on and off */}
-                {/* <Checkbox
-                    checked={notificationsEnabled}
-                    onChange={e => setNotificationsEnabled(e.currentTarget.checked)}
-                    checkmarkType={STYLE_TYPE.toggle_round}>
-                    Enable Push Notifications
-                </Checkbox> */}
             </Card>
         </div>
     );

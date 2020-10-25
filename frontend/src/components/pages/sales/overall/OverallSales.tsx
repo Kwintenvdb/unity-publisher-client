cea9376a9757bfa8121135aaf8f3a83d35a2a201
kwintenvdb <kwintenvdberghe@gmail.com>
2020-10-25 23:20:22 +0100

    Added average monthly revenue

--- frontend/src/components/pages/sales/overall/OverallSales.tsx
+++ frontend/src/components/pages/sales/overall/OverallSales.tsx
@@ -7,18 +7,38 @@ import { SalesDto } from '@shared/SalesDto';
 import { formatCurrency } from 'src/utils/formatCurrency';
 import { useStyletron } from 'baseui';
 import { H3 } from 'baseui/typography';
+import { stringify } from 'querystring';
 
 export function OverallSales() {
     const { data: allSales } = useQuery('allSales', getAllSales);
     const [css, theme] = useStyletron();
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const months = Array.from(new Set(allSales?.map(s => s.month))).sort();
+
+    const salesGross = (sales: SalesDto[] | undefined) => {
+        return sales?.reduce((sum: number, value: SalesDto) => sum + value.gross, 0) ?? 0;
+    };
 
     const salesTotalGross = () => {
-        return allSales?.reduce((sum: number, value: SalesDto) => sum + value.gross, 0) ?? 0;
+        return salesGross(allSales);
     };
 
     const salesTotalNet = () => {
         return salesTotalGross() * 0.7;
+    }
+
+    const averageGrossLastYear = () => {
+        if (!allSales) return 0;
+
+        const last12Months = months.slice(-12);
+        const salesLastYear = last12Months.flatMap(month => {
+            return allSales.filter(sale => sale.month === month);
+        });
+        return salesGross(salesLastYear) / last12Months.length;
+    };
+
+    const averageNetLastYear = () => {
+        return averageGrossLastYear() * 0.7;
     }
 
     useEffect(() => {
@@ -109,22 +129,41 @@ export function OverallSales() {
     return (
         <div className="mt-5">
             <div className="mb-6">
-                <Card title="All Time Revenue">
-                    <div className="flex">
-                        <div className="w-1/2">
-                            <H3 color="contentAccent">
-                                {formatCurrency(salesTotalGross())}
-                                <span className="text-lg font-normal ml-1"> gross</span>
-                            </H3>
+                <div className="grid xl:grid-cols-2 gap-6">
+                    <Card title="All Time Revenue">
+                        <div className="flex">
+                            <div className="w-1/2">
+                                <H3 color="contentAccent">
+                                    {formatCurrency(salesTotalGross())}
+                                    <span className="text-lg font-normal ml-1"> gross</span>
+                                </H3>
+                            </div>
+                            <div className="w-1/2">
+                                <H3 color="contentPositive">
+                                    {formatCurrency(salesTotalNet())}
+                                    <span className="text-lg font-normal ml-1"> net</span>
+                                </H3>
+                            </div>
                         </div>
-                        <div className="w-1/2">
-                            <H3 color="contentPositive">
-                                {formatCurrency(salesTotalNet())}
-                                <span className="text-lg font-normal ml-1"> net</span>
-                            </H3>
+                    </Card>
+
+                    <Card title="Avg. Monthly Revenue in the Last Year">
+                        <div className="flex">
+                            <div className="w-1/2">
+                                <H3 color="contentAccent">
+                                    {formatCurrency(averageGrossLastYear())}
+                                    <span className="text-lg font-normal ml-1"> gross</span>
+                                </H3>
+                            </div>
+                            <div className="w-1/2">
+                                <H3 color="contentPositive">
+                                    {formatCurrency(averageNetLastYear())}
+                                    <span className="text-lg font-normal ml-1"> net</span>
+                                </H3>
+                            </div>
                         </div>
-                    </div>
-                </Card>
+                    </Card>
+                </div>
             </div>
             <Card title="Sales Over Time">
                 <canvas ref={canvasRef} height="200" width="400"></canvas>
